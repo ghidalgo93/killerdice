7e918719e5cd5a3232506c69a1b4bc9729f05b67
Gerardo Hidalgo <gerardo@pop-os.localdomain>
2020-07-31 14:03:17 -0600

    was able to implement kind of dice, number of dice, and modifiers

--- helloworld.py
+++ helloworld.py
@@ -1,12 +1,19 @@
 import random
 
-dsize = int(input("What die would you like to roll?"))
-dnum = int(input("How many of those dice would you like to roll?"))
+dsize = int(input("What kind of dice would you like to roll? "))
+dnum = int(input("How many of those dice would you like to roll? "))
+modQuestion = input("Would you like to add a modifier (y/n)? ")
+if modQuestion == 'y':
+    mod = int(input("What would you like to add/subtract? "))
 
 dicelist = []
+modlist = []
 
 for i in range(0,dnum):
     n = random.randint(1,dsize)
+    if modQuestion == 'y':
+        modlist.append(n+mod)
     dicelist.append(n)
 
 print(dicelist)
+print(modlist)
